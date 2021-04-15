61ad5d4e0175b487fffc839e44ab0d9a386c33ad
liuzikai <zikai.liu@outlook.com>
2021-04-15 15:33:44 +0800

    Add ChargerManagerUnitTest.

---
+++ tests/ChargerManagerUnitTest.cpp
@@ -0,0 +1,32 @@
+//
+// Created by liuzikai on 4/15/21.
+//
+
+#include "ChargerManager.h"
+#include <iostream>
+
+ChargerManager chargerManager;
+
+int main() {
+
+    while (true) {
+        for (int i = 0; i < ChargerManager::CHARGER_COUNT; i++) {
+            std::cout << "i: ";
+            switch (chargerManager.getChargerStatus(i)) {
+                case ChargerManager::UNKNOWN:
+                    std::cout << "UNKNOWN         ";
+                    break;
+                case ChargerManager::NOT_CHARGING:
+                    std::cout << "NOT_CHARGING    ";
+                    break;
+                case ChargerManager::CHARGING:
+                    std::cout << "CHARGING        ";
+                    break;
+            }
+        }
+        std::cout << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+
+    return 0;
+}
